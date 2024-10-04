ee1230ec388765d13822bd4c3ef744ffc546f16d
Бермет Усубалиева <usubalievabermet@gmail.com>
2024-10-04 11:00:45 +0600

    feat: add pizza from mockAPI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/app.scss'
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json'
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://66ff70bd2b9aac9c997f4634.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr))
+  }, [])
 
   return (
     <div className="App">
@@ -15,13 +21,13 @@ function App() {
         <div className="content">
           <div className="container">
             <div className="content__top">
-              {/* <Categories /> */}
+              <Categories />
               <Sort />
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
               {
-                pizzas.map((el) => (
+                items.map((el) => (
                   <PizzaBlock key={el.id} {...el} />
                 ))
               }
