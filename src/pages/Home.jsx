97a368ef2c3b0950bb1dec1f8aaf66013aa76220
Бермет Усубалиева <usubalievabermet@gmail.com>
2024-10-11 13:18:52 +0600

    fix: adaptive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,11 @@ const Home = () => {
                 setItems(arr)
                 setIsLoading(false)
             })
+        window.scrollTo(0, 0)
     }, [])
 
     return (
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories />
                 <Sort />
@@ -32,7 +33,7 @@ const Home = () => {
                         : items.map((el) => <PizzaBlock key={el.id} {...el} />)
                 }
             </div>
-        </>
+        </div>
     );
 };
 
